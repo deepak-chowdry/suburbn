9aad8ad137d49e91f9342ac824d114b98fd79a23
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-08-08 12:09:08 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,176 +1,150 @@
-'use client'
-import Image from 'next/image';
-import React, { useEffect, useRef } from 'react'
-import { Poppins } from "next/font/google";
+"use client";
+import Image from "next/image";
+import React, { useRef } from "react";
 import gsap from "gsap";
-import Link from 'next/link';
-import { useGSAP } from '@gsap/react';
-
-const popp = Poppins({
-    subsets: ["latin"],
-    weight: ['100', '200', '800']
-});
+import { useGSAP } from "@gsap/react";
+import Link from "next/link";
 
 const Navbar = () => {
-    const menuBtnRef = useRef(null);
-    const menuRef = useRef(null);
-    const path1 = useRef(null);
-    const path2 = useRef(null);
+  const menuBtnRef = useRef();
+  const path1 = useRef();
+  const path2 = useRef();
+  const logoRef = useRef();
 
+  useGSAP(() => {
     let isMenuOpen = false;
-    const searchIcon = useRef(null);
-    const searchBox = useRef(null);
 
-    let isSearchOpen = false;
+    menuBtnRef.current.addEventListener("click", function () {
+      if (isMenuOpen) {
+        gsap.to(path1.current, {
+          attr: { d: "M 0 5 L 25 5" },
+        });
 
-    useGSAP(() => {
-        const handleMenuClick = () => {
-            if (isMenuOpen) {
-                gsap.to(path1.current, {
-                    attr: { d: "M 0 5 L 25 5" },
-                });
-                gsap.to(path2.current, {
-                    attr: { d: "M 0 13 L 25 13" },
-                });
-                gsap.to(menuRef.current, {
-                    top: '-384px',
-                    display: 'none',
-                    opacity: 0,
-                    duration: 0.5,
-                    delay: 0.2,
-                });
+        gsap.to(path2.current, {
+          attr: { d: "M 0 13 L 25 13" },
+        });
+        gsap.to(logoRef.current, {
+          attr: { fill: "black" },
+          delay: 0.6,
+        });
+        gsap.to("#menu", {
+          height: 0,
+          opacity: 0,
+          duration: 0.3,
+          delay: 0.5,
+        });
+        gsap.to("#menubtn", {
+          attr: {
+            stroke: "#000",
+          },
+          delay: 0.6,
+        });
+       
+        gsap.to("#links *", {
+          opacity: 0,
+          transform: "translateY(40px)",
+          duration: 0.2,
+          stagger: 0.02,
+          ease: "back.in",
+        });
+        gsap.to("#buttons", {
+          background: "#fff",
+          delay: 0.7,
+        });
 
-                isMenuOpen = false;
-            } else {
-                gsap.to(path1.current, {
-                    attr: { d: "M 0 0 L 17 16" },
-                });
-                gsap.to(path2.current, {
-                    attr: { d: "M 0 16 L 17 1" },
-                });
-                gsap.to(menuRef.current, {
-                    top: 0,
-                    display: 'flex',
-                    opacity: 1,
-                    duration: 0.5,
-                    delay: 0.1,
-                });
+        isMenuOpen = false;
+      } else {
+        gsap.to(path1.current, {
+          attr: { d: "M 0 0 L 17 16" },
+        });
 
-                isMenuOpen = true;
-            }
-        };
-        const handleSearchClick = () => {
-            if (isSearchOpen) {
-                gsap.to(searchBox.current, {
-                    height: 0,
-                    opacity: 0,
-                    duration: 0.5,
-                });
-                gsap.to('#menu', {
-                    width: '50%',
-                    duration: 0.8,
-                })
-                gsap.to('#menubtn', {
-                    opacity: 1,
-                    duration: 0.6,
-                })
-                gsap.to('#logo', {
-                    width: 'auto',
-                    duration: 0.7,
-                })
-                isSearchOpen = false;
-            } else {
-                gsap.to(searchBox.current, {
-                    height: '340px',
-                    opacity: 1,
-                    duration: 0.5,
-                });
-                gsap.to('#menu', {
-                    width: 0,
-                    duration: 0.8,
-                })
-                gsap.to('#menubtn', {
-                    opacity: 0,
-                    duration: 0.6,
-                })
-                gsap.to('#logo', {
-                    width: '50%',
-                    duration: 0.7,
-                })
-                isSearchOpen = true;
-            }
-        };
+        gsap.to(path2.current, {
+          attr: { d: "M 0 16 L 17 1" },
+        });
+        gsap.to(logoRef.current, {
+          attr: { fill: "white" },
+        });
+        gsap.to("#menu", {
+          height: "55vh",
+          opacity: 1,
+          duration: 0.3,
+          delay: 0.3,
+        });
+        gsap.to("#menubtn", {
+          attr: {
+            stroke: "#fff",
+          },
+        });
+       
+        gsap.to("#links *", {
+          opacity: 1,
+          transform: "translateY(0)",
+          duration: 0.4,
+          delay: 0.3,
+          stagger: 0.05,
+          ease: "back.in",
+        });
+        gsap.to("#buttons", {
+          background: "transparent"
+        });
 
-        const searchButton = searchIcon.current;
-        searchButton.addEventListener('click', handleSearchClick);
+        isMenuOpen = true;
+      }
+    });
+  });
+  return (
+    <>
+      <div className="flex items-center justify-center h-20 fixed w-full z-30">
+        <div className="flex items-center justify-between w-[93%]">
+          <svg
+            ref={logoRef}
+            width="80"
+            height="80"
+            viewBox="0 0 37 10"
+            fill="black"
+            xmlns="http://www.w3.org/2000/svg"
+            className="z-10"
+          >
+            <path d="M0.266 11L2.058 0.444H4.76L6.902 6.086L9.17 0.444H11.9L13.496 11H10.752L9.982 4.924H9.954L7.42 11H6.328L3.906 4.924H3.878L2.996 11H0.266Z"/>
+          </svg>
 
+          <div id="buttons" className="flex items-center justify-center rounded-full z-10">
+            <svg
+              ref={menuBtnRef}
+              width="22"
+              height="16"
+              id="menubtn"
+              stroke="#000"
+              className="cursor-pointer"
+            >
+              <path ref={path1} d="M 0 5 L 25 5" />
+              <path ref={path2} d="M 0 13 L 25 13" />
+            </svg>
+          </div>
 
-        const menuButton = menuBtnRef.current;
-        menuButton.addEventListener('click', handleMenuClick);
-
-        return () => {
-            menuButton.removeEventListener('click', handleMenuClick);
-            searchButton.removeEventListener('click', handleSearchClick);
-
-        };
-    }, []);
-    return (
-        <>
-            <div className='flex items-center justify-center h-[76px] shadow-sm sticky top-0 bg-zinc-50'>
-                <div className='flex items-center justify-between w-11/12'>
-                    <div
-                        id="menu"
-                        className="flex items-center justify-start rounded-full w-1/2"
-                    >
-                        <svg
-                            ref={menuBtnRef}
-                            width="22"
-                            height="16"
-                            id="menubtn"
-                            stroke="#000"
-                            className="cursor-pointer z-20"
-                        >
-                            <path ref={path1} d="M 0 5 L 25 5" />
-                            <path ref={path2} d="M 0 13 L 25 13" />
-                        </svg>
-                        {/* <div ref={menuRef} className={`hidden items-center justify-center w-full absolute h-96 left-0 -top-96 opacity-0 bg-zinc-100 shadow-sm z-10 ${popp.className}`}>
-                            <div className='flex flex-col justify-end h-5/6 w-11/12'>
-                                <div className='flex flex-col items-end text-5xl uppercase space-y-2 h-2/3 w-full'>
-                                    <Link href={'/store'}>Store</Link>
-                                    <Link href={'/'}>Contact</Link>
-                                    <Link href={'/'}>Sign In</Link>
-                                </div>
-                                <div>
-                                    <h3>INR ₹ | India</h3>
-                                </div>
-                            </div>
-                        </div> */}
-                    </div>
-                    <div id="logo" className='w-auto'>
-                        <Image src={'/Img/logo-black.svg'} width={100} height={100} alt='logo' priority className='w-28' />
-                    </div>
-                    <div className='flex items-center justify-end w-1/2 space-x-5'>
-                        <div className=''>
-                            <Image ref={searchIcon} src={'/Img/search.svg'} width={23} height={23} alt='search' priority />
-                            {/* <div ref={searchBox} className="absolute w-full h-0 overflow-hidden bg-zinc-50 right-0 top-16 flex items-center justify-center">
-                                <div className="flex flex-col space-y-2 h-[90%] justify-start w-11/12">
-                                    <div className="w-full h-14 border border-stone-400 px-4">
-                                        <input type="text" className="w-full h-full bg-transparent outline-none border-none" placeholder="Search" />
-                                    </div>
-                                    <div>
-                                        <p>Your Search results</p>
-                                    </div>
-                                </div>
-                            </div> */}
-                        </div>
-                        <Link href={'/cart'}>
-                            <Image id="cart-icon" src={'/Img/bag.svg'} width={30} height={30} alt='bag' priority />
-                        </Link>
-                    </div>
-                </div>
+          <div
+            id="menu"
+            className="w-full h-0 absolute left-0 top-0 bg-[#111] text-white flex items-center justify-center opacity-0"
+          >
+            <div
+              id="links"
+              className="flex flex-col items-end justify-end w-[95%] h-[95%] text-[14vw] leading-[14.5vw] md:text-[5vw] md:leading-[6vw] uppercase font-black"
+            >
+              <Link className="opacity-0 translate-y-10" href={"/admin"}>
+                Admin
+              </Link>
+              <Link className="opacity-0 translate-y-10" href={"/"}>
+                Catalog
+              </Link>
+              <Link className="opacity-0 translate-y-10" href={"/signin"}>
+                Sign In
+              </Link>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
