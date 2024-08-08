5ce69808abbcc63be3fa5520faba4b4459f0f67f
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-08-08 12:13:57 +0530

    gd

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Navbar from "@/Components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+     <Navbar />
       <div className="flex items-center justify-center h-screen">
         Welcome to suburbn.
       </div>
