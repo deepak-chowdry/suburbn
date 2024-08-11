4d774171984624f84a9a20242d6024fed6983cd2
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-08-11 09:57:04 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='flex items-center justify-center h-20'>
+        <div className='flex items-center justify-between w-11/12'>
+          <h3>menu</h3>
+          <h2>Mahadev</h2>
+          <h3>search</h3>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
